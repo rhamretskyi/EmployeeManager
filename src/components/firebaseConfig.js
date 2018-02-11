3489f688a63d8f9cd1953c1bb49b862f9e487723
Roman Hamretskyi <roman.hamretskyi@devup-labs.com>
2018-02-11 13:09:35 +0200

    initial commit

---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBgleoOqtmZgZZ0oD7f12wmU9hvaxsIgeU",
+    authDomain: "vuefs-prod-e96b6.firebaseapp.com",
+    databaseURL: "https://vuefs-prod-e96b6.firebaseio.com",
+    projectId: "vuefs-prod-e96b6",
+    storageBucket: "vuefs-prod-e96b6.appspot.com",
+    messagingSenderId: "3496231633"
+}
